01c5551456f53424c613a017147c26f590eb4501
Eduardo Valencia <8025918@gmail.com>
2020-06-27 16:20:46 -0700

    Updates

---
+++ precache-manifest.56859123a9e29d27f81b1f6f4404899b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3892343b69906b27d0fabac2ca7c0ace",
+    "url": "/learning-dashboard/index.html"
+  },
+  {
+    "revision": "e31aa4629993d62e928d",
+    "url": "/learning-dashboard/static/css/main.763d8571.chunk.css"
+  },
+  {
+    "revision": "c651965b2c73c373484b",
+    "url": "/learning-dashboard/static/js/2.50237fb8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learning-dashboard/static/js/2.50237fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31aa4629993d62e928d",
+    "url": "/learning-dashboard/static/js/main.18efa6c6.chunk.js"
+  },
+  {
+    "revision": "ae4292a0ce4dc0753f83",
+    "url": "/learning-dashboard/static/js/runtime-main.39db37a1.js"
+  },
+  {
+    "revision": "a663b11926d811a3160ce38c47d63ab6",
+    "url": "/learning-dashboard/static/media/alert-circle.a663b119.svg"
+  },
+  {
+    "revision": "8dc3ae442f18cb0e446565b86e442446",
+    "url": "/learning-dashboard/static/media/arrow-down.8dc3ae44.svg"
+  },
+  {
+    "revision": "21a3809438a0e54dc307e2a2e9869839",
+    "url": "/learning-dashboard/static/media/arrow-right.21a38094.svg"
+  },
+  {
+    "revision": "3c9751b2030eda7b1a67523d90dba226",
+    "url": "/learning-dashboard/static/media/chat.3c9751b2.svg"
+  },
+  {
+    "revision": "11608ad097080982642a5e7388f80ed1",
+    "url": "/learning-dashboard/static/media/classes.11608ad0.svg"
+  },
+  {
+    "revision": "605764d49b7de653f6cd542f99d20f48",
+    "url": "/learning-dashboard/static/media/dashboard.605764d4.svg"
+  },
+  {
+    "revision": "ff7de2e93cd3856f3880b84a96828a1e",
+    "url": "/learning-dashboard/static/media/envelope.ff7de2e9.svg"
+  },
+  {
+    "revision": "770afc730b829d1952c43d899e4571e4",
+    "url": "/learning-dashboard/static/media/learning.770afc73.svg"
+  },
+  {
+    "revision": "f604827c5bccaa8b091f4c5b1bff5e17",
+    "url": "/learning-dashboard/static/media/logout.f604827c.svg"
+  },
+  {
+    "revision": "de58ef7a6c1c8b29d43cdcd208b5dbe0",
+    "url": "/learning-dashboard/static/media/notification.de58ef7a.svg"
+  },
+  {
+    "revision": "d8c24584e5080424c85f8aa9226d479f",
+    "url": "/learning-dashboard/static/media/plan.d8c24584.svg"
+  },
+  {
+    "revision": "205737f4dfd9b6cc474ebecc18ff95dc",
+    "url": "/learning-dashboard/static/media/profile.205737f4.svg"
+  },
+  {
+    "revision": "c1c5ec96de0a8a997978daf3cb4aa8bd",
+    "url": "/learning-dashboard/static/media/resources.c1c5ec96.svg"
+  },
+  {
+    "revision": "1bcd07470ba141686f31b468ba6f4be7",
+    "url": "/learning-dashboard/static/media/search.1bcd0747.svg"
+  },
+  {
+    "revision": "1587dfb689f867b1c5350c0c442a78dd",
+    "url": "/learning-dashboard/static/media/settings.1587dfb6.svg"
+  },
+  {
+    "revision": "d471d44d986eb7e245adc458781a47df",
+    "url": "/learning-dashboard/static/media/upgrade.d471d44d.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/learning-dashboard/precache-manifest.56859123a9e29d27f81b1f6f4404899b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learning-dashboard/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
